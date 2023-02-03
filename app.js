8d55792f591c94e35b225a242d7c4db4d91d4569
Suhail Khan <56758947+sksuhailkhan2017@users.noreply.github.com>
2023-02-03 18:12:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-     var url = "/api/predict_home_price";
+     var url = "/127.0.0.1:5000/predict_home_price";
        
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -42,7 +42,7 @@ function getBathValue() {
   
   function onPageLoad() {
     console.log( "document loaded" );
-    var url = "/api/get_location_names";
+    var url = "/127.0.0.1:5000/get_location_names";
     
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
